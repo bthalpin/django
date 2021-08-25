6b914008c6c49511e9e13e2948f0bc4f7eabdd69
bthalpin <brian.t.halpin@gmail.com>
2021-08-25 15:15:25 -0400

    update

--- django_level_five/learning_users/basic_app/forms.py
+++ django_level_five/learning_users/basic_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from basic_app.models import UserProfileInfo
 
-class UseForm(forms.ModelForm):
+class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta():
@@ -13,3 +13,4 @@ class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = ('portfolio_site','profile_pic')
+     
--- django_level_five/learning_users/learning_users/settings.py
+++ django_level_five/learning_users/learning_users/settings.py
@@ -138,6 +138,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/basic_app/user_login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
